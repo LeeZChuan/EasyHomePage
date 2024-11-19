95ba0affbeb410c69209be0f85043217637cae8f
LeeZChuan <LeeZChuan@users.noreply.github.com>
2024-11-19 15:00:17 +0000

    deploy: 165804f7ca453a98189bb9535dae085c64fb03a5

---
+++ assets/ydy-CX4jjMUe.js
@@ -0,0 +1,12 @@
+const n={date:"2022-06",title:"前端开发工程师",id:"ydy",company:"易点云",range:"2022.06 - 2023.10",url:"https://edianyun.com/"},s=`<p>易点云是一家硬件租赁公司，作为前端开发工程师，负责公司易盘点sass平台与小程序的开发。</p>
+<ul>
+<li>负责易盘点sass平台的前端开发</li>
+<li>负责公司内部管理系统的开发</li>
+<li>负责易盘点的小程序多端Taro开发</li>
+</ul>
+`,t=`易点云是一家硬件租赁公司，作为前端开发工程师，负责公司易盘点sass平台与小程序的开发。
+
+- 负责易盘点sass平台的前端开发
+- 负责公司内部管理系统的开发
+- 负责易盘点的小程序多端Taro开发
+`;export{n as attributes,s as html,t as markdown};
