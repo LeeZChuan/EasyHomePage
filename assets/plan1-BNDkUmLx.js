fab20e31c920a7495d90daf3b26f8ca01c4c424b
LeeZChuan <LeeZChuan@users.noreply.github.com>
2024-11-19 08:09:27 +0000

    deploy: ab060648c358ab60405d2dd75dcb13ef3e88f540

---
+++ assets/plan1-BNDkUmLx.js
@@ -0,0 +1,1 @@
+const t={date:1,Title:"一起骑行徒步",Subtitle:"约他一起骑车，一起去看看世界",Includes:["路线建议服务","停歇时陪同抽烟","吃饭时一起吹牛","让你教他骑车"],Excludes:["不提供公路车","住宿费用自理"],Price:"5 $"},e="",c="";export{t as attributes,e as html,c as markdown};
---
+++ assets/plan3-BzLOdF4_.js
@@ -0,0 +1,1 @@
+const t={date:3,Title:"正经技术咨询",Subtitle:"提供短期或长期的技术咨询服务",Includes:["技术框架设计咨询","效率提升咨询","可以帮你加油","可以陪同上厕所","包含前两种服务"],Price:"99 $"},e="",c="";export{t as attributes,e as html,c as markdown};
