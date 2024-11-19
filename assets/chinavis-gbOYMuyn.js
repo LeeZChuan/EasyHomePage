251d4431378010ff73dae3b70d2ad5102da882fa
LeeZChuan <LeeZChuan@users.noreply.github.com>
2024-11-19 06:31:31 +0000

    deploy: 911d52569278eb7b13522105fd2da266d335ceb7

--- assets/chinavis-gbOYMuyn.js
+++
@@ -1,3 +0,0 @@
-const t={date:"2019",title:"实时监控场馆可视化系统",cover:"./images/ChinaVis2019_Poster.png",url:"https://chinavis.org/2019/challenge.html",tags:["可视化"]},n=`<p>大学期间，参与Chinavis2019可视化比赛，设计并制作了实时可视化监控平台。</p>
-`,s=`大学期间，参与Chinavis2019可视化比赛，设计并制作了实时可视化监控平台。
-`;export{t as attributes,n as html,s as markdown};
