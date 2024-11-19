d6328d52058c8abae0b69dd983a4aec80b96bf44
LeeZChuan <LeeZChuan@users.noreply.github.com>
2024-11-19 08:31:41 +0000

    deploy: a605e40baf2d5837e6298d60aab756abb1331d74

---
+++ assets/chinavis-BmJeTDzA.js
@@ -0,0 +1,3 @@
+const t={date:"2019-05",title:"ChinaVis2019-智能场馆安全运营",url:"https://chinavis.org/2019/challenge.html",tags:["Javascript","Vue","D3.js","Data Analysis","Echarts","Visualization"]},a=`<p>本项目是本科期间参加国内ChinaVis2019的参赛作品，并获得一等奖。</p>
+`,s=`本项目是本科期间参加国内ChinaVis2019的参赛作品，并获得一等奖。
+`;export{t as attributes,a as html,s as markdown};
