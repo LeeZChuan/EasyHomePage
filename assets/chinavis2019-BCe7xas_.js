05d2c4b13e35301ace1d2fba0624e8e3d7dd3e44
LeeZChuan <LeeZChuan@users.noreply.github.com>
2024-11-23 07:52:31 +0000

    deploy: 8a45699f4541aae3c815ba34ebd0d4bb618ac5fd

---
+++ assets/chinavis2019-BCe7xas_.js
@@ -0,0 +1,3 @@
+const t={date:"2019-06",title:"SCVis:基于传感器数据的异常可视化分析",url:"https://chinavis.org/2019/challenge.html",tags:["Javascript","D3.js","Echarts","Visualization"]},s=`<p>本科期间，参加ChinaVis2019，第一次使用自制平台工具进行可视化分析。</p>
+`,a=`本科期间，参加ChinaVis2019，第一次使用自制平台工具进行可视化分析。
+`;export{t as attributes,s as html,a as markdown};
---
+++ assets/chinavis2022-CFvzVlID.js
@@ -0,0 +1,3 @@
+const t={date:"2022-06",title:"BDVis：数据安全可视分析",url:"https://chinavis.org/2022/challenge.html",tags:["React","Neo4j"]},e=`<p>ChinaVis2022，主要使用Neo4j图数据库进行分析，使用官方提供的工具进行可视化分析展示。</p>
+`,n=`ChinaVis2022，主要使用Neo4j图数据库进行分析，使用官方提供的工具进行可视化分析展示。
+`;export{t as attributes,e as html,n as markdown};
