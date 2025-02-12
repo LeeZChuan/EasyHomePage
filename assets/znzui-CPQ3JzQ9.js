063672e949de08e8e161d32010b271b51b884ac3
LeeZChuan <LeeZChuan@users.noreply.github.com>
2025-02-12 12:56:41 +0000

    deploy: e0859b0c84aaa3859f31385bec7b19f0d1d4ba1d

---
+++ assets/znzui-CPQ3JzQ9.js
@@ -0,0 +1,3 @@
+const t={date:"2024-12",title:"组件库",cover:"github.png",url:"",tags:["组件库","封装","ElementUI"]},e=`<p>基于ElementUI的代码结构封装了一套符合业务需求的组件库,包含table、date等业务需求,发布到内网之中仅需引入项目即可使用组件,无需编写重复代码;同时使用虚拟滚动来增强具有超过 100k 个单元格表格的效能。</p>
+`,n=`基于ElementUI的代码结构封装了一套符合业务需求的组件库,包含table、date等业务需求,发布到内网之中仅需引入项目即可使用组件,无需编写重复代码;同时使用虚拟滚动来增强具有超过 100k 个单元格表格的效能。
+`;export{t as attributes,e as html,n as markdown};
