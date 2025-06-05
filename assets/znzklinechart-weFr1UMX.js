30739fed6b01ff62794a22d9e2ee13442bc344a9
LeeZChuan <LeeZChuan@users.noreply.github.com>
2025-06-05 05:26:44 +0000

    deploy: 5d8a95b3a47c245443ec1c1a3fa04c5dddfaa3ae

---
+++ assets/znzklinechart-weFr1UMX.js
@@ -0,0 +1,3 @@
+const t={date:"2025-01",title:"基于开源klinechart定制化修改的业务行情图表库",url:"",tags:["ts","pnpm","Rollup"]},a=`<p>基于<a href="https://klinecharts.com/">klinechart</a>封装的符合公司业务的行情组件,运用在app功能演示等.</p>
+`,n=`基于[klinechart](https://klinecharts.com/)封装的符合公司业务的行情组件,运用在app功能演示等.
+`;export{t as attributes,a as html,n as markdown};
---
+++ assets/znzui-DqehljCb.js
@@ -0,0 +1,6 @@
+const e={date:"2024-12",title:"znz-ui组件库",cover:"github.png",url:"",tags:["组件库","封装","ElementUI"]},t=`<p>基于ElementUI的代码结构封装了一套符合业务需求的组件库,包含table、date等业务需求,发布到内网之中仅需引入项目即可使用组件,无需编写重复代码;同时使用虚拟滚动来增强具有超过 100k 个单元格表格的效能。</p>
+<p>主要采用element-plus的方案编写znz-ui的组件,其中使用了bem语法与组件抽象定义了业务组件;</p>
+`,n=`基于ElementUI的代码结构封装了一套符合业务需求的组件库,包含table、date等业务需求,发布到内网之中仅需引入项目即可使用组件,无需编写重复代码;同时使用虚拟滚动来增强具有超过 100k 个单元格表格的效能。
+
+主要采用element-plus的方案编写znz-ui的组件,其中使用了bem语法与组件抽象定义了业务组件;
+`;export{e as attributes,t as html,n as markdown};
