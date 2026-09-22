8628f2aadf34a7630c0a177c307964f3680997c9
LeeZChuan <LeeZChuan@users.noreply.github.com>
2026-09-22 08:04:33 +0000

    deploy: 738f034d6734727374bba5587c390146b776a6f4

---
+++ assets/chinavis-DNpXRpUz.js
@@ -0,0 +1,3 @@
+const t={date:"2019",title:"SCVis:基于传感器数据的异常可视化分析",cover:"ChinaVis2019_Poster.png",url:"https://chinavis.org/2019/challenge.html",tags:["可视化"]},n=`<p>上学期间，参与ChinaVis2019可视化比赛，设计并制作了实时可视化监控平台，由于本人在2018年就参加过一届拿过优秀奖，有了部分经验，最后有幸拿了一个一等奖(入选的比赛中评分第一)。</p>
+`,s=`上学期间，参与ChinaVis2019可视化比赛，设计并制作了实时可视化监控平台，由于本人在2018年就参加过一届拿过优秀奖，有了部分经验，最后有幸拿了一个一等奖(入选的比赛中评分第一)。
+`;export{t as attributes,n as html,s as markdown};
